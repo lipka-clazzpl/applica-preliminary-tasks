a2b7e925a3667b67540376677e058aab0d0598c7
Przemek Lipka <github@clazz.pl>
2018-02-03 23:12:49 +0100

    Implementation of null checking and empty input handling.

--- src/main/java/pl/clazz/MarkupSegmentation.java
+++ src/main/java/pl/clazz/MarkupSegmentation.java
@@ -1,12 +1,24 @@
 package pl.clazz;
 
-import pl.clazz.model.Segment;
-
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.Boolean.FALSE;
 
 public class MarkupSegmentation {
 
     List<Segment> separate(String text) {
+        if(text == null) {
+            throw new IllegalArgumentException("First input parameter text of type String should not be null");
+        }
+
+        if("".equals(text)) {
+            return new ArrayList<Segment>() {
+                {
+                    add(new Segment("", FALSE));
+                }
+            };
+        }
 
         return null;
     }
